21bf1f1e8f65d4be98c44e8417cce84c6dcce534
jo <jo@aquiet.life>
2020-05-08 19:57:42 -0400

    cleaned up code

--- day_8_check_straight_line.py
+++ day_8_check_straight_line.py
@@ -1,12 +1,10 @@
 class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        
         first_coordinate = coordinates[0]
         second_coordinate = coordinates[1]
+
         delta_y = second_coordinate[1] - first_coordinate[1]
-
         delta_x = second_coordinate[0] -  first_coordinate[0]
-
         
         initial_slope = delta_y / delta_x if delta_x else 0
         
